bd8ea48f77c45ef4c33ba23bbb37c399596a12df
Jens Ettl <ettljens@gmail.com>
2023-10-27 14:26:07 +0200

    updated skills elements

--- public/src/components/skills/Skills.jsx
+++ public/src/components/skills/Skills.jsx
@@ -1,96 +1,111 @@
-import React from "react";
-import ProgressBar from "../../chip/ProgressBar";
-import SkillBox from "../../chip/SkillBox";
+import React from 'react';
+import ProgressBar from '../../chip/ProgressBar';
+import SkillBox from '../../chip/SkillBox';
 
-import { IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
-
-
+import { IoLogoPython } from 'react-icons/io';
+import { AiOutlineTeam } from 'react-icons/ai';
+import { BsFillDatabaseFill } from 'react-icons/bs';
+import { BiMath } from 'react-icons/bi';
 import {
-  SiJavascript,
-  SiTailwindcss,
-  SiMongodb,
-  SiExpress,
-} from "react-icons/si";
-import { GrCode } from "react-icons/gr";
-import { SiNextdotjs } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { IoLogoNodejs } from "react-icons/io";
+	SiJavascript,
+	SiAnaconda,
+	SiJupyter,
+	SiTensorflow,
+} from 'react-icons/si';
 
+import { MdSyncProblem } from 'react-icons/md';
+import { RiFileExcel2Fill } from 'react-icons/ri';
+SiTensorflow;
 const Skills = ({ darkMode }) => {
-  return (
-    <div id="skills">
-      <div className=" container m-auto  mt-16">
-        {/* heading */}
-        <div data-aos="fade-up" className="relative mb-5">
-          <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
-            My Skills
-          </h3>
-          <span className="h-[1.1px] right-0 absolute w-[90%] bg-gray-300 block"></span>
-        </div>
-        {/* content*/}
-        <div className="flex md:flex-col ">
-          <div className="left flex-1 w-full">
-            <p
-              data-aos="fade-up"
-              className=" text-gray-700 font-medium w-[100%]"
-            >
-              Here are my skills.
-            </p>
-            {/* left box */}
-            <div
-              data-aos="zoom-in"
-              className="progress flex items-center h-[100%] justify-end md:justify-center"
-            >
-              <div className=" flex flex-col gap-6  w-3/4  my-5 md:w-[90%]">
-                <ProgressBar logo={<IoLogoHtml5 />} name={"HTML"} value={95} />
-                <ProgressBar logo={<IoLogoCss3 />} name={"CSS"} value={88} />
-                <ProgressBar
-                  logo={<SiJavascript />}
-                  name={"Javascript"}
-                  value={80}
-                />
-                <ProgressBar logo={<FaReact />} name={"React Js"} value={80} />
-                <ProgressBar
-                  logo={<SiTailwindcss />}
-                  name={"Tailwind CSS"}
-                  value={80}
-                />
-              </div>
-            </div>
-          </div>
-          {/* right box */}
-          <div className="right relative flex-1 flex flex-wrap p-5 gap-10 items-center justify-center sm:w-full">
-            <div className="first2 flex flex-col gap-10">
-              <SkillBox
-                logo={<IoLogoNodejs />}
-                black={"white"}
-                white={"black"}
-                skill={"Node Js"}
-              />
-              <SkillBox
-                logo={<SiMongodb />}
-                black={"white"}
-                white={"black"}
-                skill={"MongoDB"}
-              />
-            </div>
-            <div className="last2 flex flex-col gap-10">
-              <SkillBox
-                logo={<SiExpress />}
-                black={"black"}
-                white={"white"}
-                skill={"Express Js"}
-              />
-              <SkillBox
-                className=""
-                logo={
-                  <GrCode/>
-                }
-                black={"black"}
-                white={"white"}
-                skill={"C++"}
-              />
-              {/* <SkillBox
+	return (
+		<div id='skills'>
+			<div className=' container m-auto  mt-16'>
+				{/* heading */}
+				<div data-aos='fade-up' className='relative mb-5'>
+					<h3 className=' text-3xl font-black text-gray-400 sm:text-2xl'>
+						My Skills
+					</h3>
+					<span className='h-[1.1px] right-0 absolute w-[90%] bg-gray-300 block'></span>
+				</div>
+				{/* content*/}
+				<div className='flex md:flex-col '>
+					<div className='left flex-1 w-full'>
+						<p
+							data-aos='fade-up'
+							className=' text-gray-700 font-medium w-[100%]'
+						>
+							Here are my skills.
+						</p>
+						{/* left box */}
+						<div
+							data-aos='zoom-in'
+							className='progress flex items-center h-[100%] justify-end md:justify-center'
+						>
+							<div className=' flex flex-col gap-6  w-3/4  my-5 md:w-[90%]'>
+								<ProgressBar
+									logo={<IoLogoPython />}
+									name={'Python'}
+									value={95}
+								/>
+								<ProgressBar
+									logo={<BiMath />}
+									name={'Maths & Statistics'}
+									value={85}
+								/>
+								<ProgressBar
+									logo={<BsFillDatabaseFill />}
+									name={'SQL'}
+									value={75}
+								/>
+								<ProgressBar
+									logo={<SiJavascript />}
+									name={'Javascript (React)'}
+									value={70}
+								/>
+								<ProgressBar
+									logo={<MdSyncProblem />}
+									name={'Problem Solving'}
+									value={85}
+								/>
+								<ProgressBar
+									logo={<AiOutlineTeam />}
+									name={'Teamwork & Communication'}
+									value={85}
+								/>
+							</div>
+						</div>
+					</div>
+					{/* right box */}
+					<div className='right relative flex-1 flex flex-wrap p-5 gap-10 items-center justify-center sm:w-full'>
+						<div className='first2 flex flex-col gap-10'>
+							<SkillBox
+								logo={<RiFileExcel2Fill />}
+								black={'white'}
+								white={'black'}
+								skill={'Excel'}
+							/>
+							<SkillBox
+								logo={<SiTensorflow />}
+								black={'white'}
+								white={'black'}
+								skill={'Tensorflow'}
+							/>
+						</div>
+						<div className='last2 flex flex-col gap-10'>
+							<SkillBox
+								logo={<SiAnaconda />}
+								black={'black'}
+								white={'white'}
+								skill={'Anaconda'}
+							/>
+							<SkillBox
+								className=''
+								logo={<SiJupyter />}
+								black={'black'}
+								white={'white'}
+								skill={'Jupyter Notebooks'}
+							/>
+							{/* <SkillBox
                 className=""
                 logo={
                   <SiNextdotjs className=" text-white bg-black rounded-full h-fit border-white overflow-hidden" />
@@ -99,12 +114,12 @@ const Skills = ({ darkMode }) => {
                 white={"white"}
                 skill={"Next Js"}
               /> */}
-            </div>
-          </div>
-        </div>
+						</div>
+					</div>
+				</div>
 
-        {/* icons */}
-        {/* <div
+				{/* icons */}
+				{/* <div
           data-aos="fade-up"
           data-aos-duration="1200"
           className=" container m-auto flex items-center justify-center mt-8 gap-8 md:gap-5"
@@ -147,9 +162,9 @@ const Skills = ({ darkMode }) => {
           <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/nodejs.png"/>
           <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/>
         </div> */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 export default Skills;
